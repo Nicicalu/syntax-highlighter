0b04c79553660f89671842cbdc75387cab277b7a
Nicolas Caluori <52490746+Nicicalu@users.noreply.github.com>
2023-05-25 14:37:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ $("#generate").click(function () {
       window.getSelection().removeAllRanges(); // clear current selection
       window.getSelection().addRange(range); // to select text
   }, 100);
+  
+  //Save language
+  document.cookie = "lang=" + lang + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 });
